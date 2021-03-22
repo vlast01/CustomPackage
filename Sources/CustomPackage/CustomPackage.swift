14c9d469852d8e16e9cb42cf90920a42fd347f9e
Vladislav <rct.stankevivs@gmail.com>
2021-03-22 12:40:47 +0300

    new version

--- Sources/CustomPackage/CustomPackage.swift
+++ Sources/CustomPackage/CustomPackage.swift
@@ -5,11 +5,14 @@ public struct USPerson {
     var name: String?
     var surname: String?
     var age: Int?
+    var isSmoking: Bool?
     
-    public init(name: String, surname: String, age: Int) {
+    
+    public init(name: String, surname: String, age: Int, isSmoking: Bool) {
         self.name = name
         self.surname = surname
         self.age = age
+        self.isSmoking = isSmoking
     }
     
 }
